977222574d8f0befb7fa160187bc504597e275a7
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-01-29 00:02:57 +0100

    created Componente Home.jsx with style introduction

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
+import Home from './Components/Home';
 
 const App = () => {
   return (
     <div>
-      <section>
-        <p className='h-screen bg-green-400 flex items-center justify-center m-12
-        text-center text-3xl p-12 md:p-16 xl:p-20'>
-          Lorem ipsum dolor sit amet, consectetur  asisdosdp elite. debiliteisdkk lsdllsd ksdja ksdjsd ooaska.
-        </p>
-      </section>
+      <Home />
     </div>
   )
 }
---
+++ my-project/src/Components/Home.jsx
@@ -0,0 +1,24 @@
+
+const Home = () => {
+  return (
+    <body className="bg-gray-50">
+        {/* header */}
+        <div>
+            <div className="container mx-auto px-4 h-24 flex items-center">
+             <div className="font-black">
+                Logo
+             </div>
+            <ul className="ml-auto">
+                <a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">How to book</a>
+                <a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">About us</a>
+                <a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Information</a>
+                <a href="" className="text-sm text-gray-600 hover:text-gray-800 hover:underline">Contact</a>
+            </ul> 
+
+            </div>
+        </div>
+    </body>
+  )
+}
+
+export default Home
