bbfbd9e8b7f6d896cfd2b8eb657063fdf8efba0e
Tarcísio Menezes <tarcisiomenezes2019@hotmail.com>
2024-01-28 21:41:22 +0100

    Vite, React and Tailwind installed and all necessary tools.

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <section>
+        <p className='h-screen bg-green-400 flex items-center justify-center m-12
+        text-center text-3xl p-12 md:p-16 xl:p-20'>
+          Lorem ipsum dolor sit amet, consectetur  asisdosdp elite. debiliteisdkk lsdllsd ksdja ksdjsd ooaska.
+        </p>
+      </section>
+    </div>
+  )
+}
+
+export default App
+
